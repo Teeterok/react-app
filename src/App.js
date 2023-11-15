28a5cf14d80cbd4be2595837d6b3930782de92f1
Bohdan <Teeteroki@gmail.com>
2023-11-15 11:32:08 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.module.scss'
+import styles from "./App.module.scss"
+function App() {
+	return (
+		<div className={styles.App}>
+			<h1>SCSS basics</h1>
+			<button  href="#">Go to channel</button>
+		</div>
+	)
+}
+
+export default App
